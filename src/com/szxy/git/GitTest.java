2a3450d9cd2f6e005e178988cd0b657259a601f1
zweros <414356480@qq.com>
2020-04-17 17:53:34 +0800

    modified README.md

---
+++ src/com/szxy/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.szxy.git;
+
+/**
+ * @Author:zwer
+ * @Date:2020/4/17 17:52
+ * @Description:com.szxy.git
+ * @Version:1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
