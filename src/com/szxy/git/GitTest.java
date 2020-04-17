fe6848847ade90cd364eb0ce148cb7f96558f8f2
zweros <414356480@qq.com>
2020-04-17 17:57:25 +0800

    zweros 修改 GitTest 文件

--- src/com/szxy/git/GitTest.java
+++ src/com/szxy/git/GitTest.java
@@ -9,5 +9,7 @@ package com.szxy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloJava");
+        System.out.println("\"remove delete\" = " + "remove delete");
     }
 }
